2479ea266ab3a7a73a9366e7a5ec485f7057518d
jedade <42737500+jedade@users.noreply.github.com>
2022-09-29 13:17:34 +0100

    Delete myscript.sh

--- myscript.sh
+++
@@ -1,9 +0,0 @@
-kubectl delete deploy backend-feed
-kubectl delete deploy backend-user
-kubectl delete deploy frontend
-kubectl delete deploy reverseproxy
-
-kubectl apply -f feed-deployment.yml
-kubectl apply -f user-deployment.yml
-kubectl apply -f frontend-deployment.yml
-kubectl apply -f reverseproxy-deployment.yml
